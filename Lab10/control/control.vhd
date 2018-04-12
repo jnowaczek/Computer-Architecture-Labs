ad8d9991c01bd55de8fcd937edb0fcf97d70e413
Julian Nowaczek <jnowaczek@users.noreply.github.com>
2018-04-11 21:00:58 -0500

    Completed control and data modules

--- Lab10/control/control.vhd
+++ Lab10/control/control.vhd
@@ -25,34 +25,88 @@ entity control is
         instruction : in std_logic_vector (31 downto 0);
         jump_decision, beq_control : out std_logic;
         MemRead, MemWrite, MemToReg : out std_logic;
-        RegDst, RegWrite, ALUOp, ALUSrc : out std_logic;
-        );
+        RegDst, RegWrite, ALUSrc : out std_logic;
+		  ALUOp : out std_logic_vector (1 downto 0));
 end control;
 
 architecture Behavioral of control is
 
 begin
     process
-        variable opcode : std_logic_vector(5 downto 0);
-begin
-    opcode(5 downto 0) := instruction(31 downto 26);
-
-    case Regdst is
-        when opcode => "100000";
-    case ALUSrc is
-        when opcode => "011010";
-    case MemToReg is
-        when opcode => "010000";
-    case RegWrite is
-        when opcode => "110010";
-    case MemRead is
-        when opcode => "010000";
-    case MemWrite is
-        when opcode => "001000";
-    case ALUOp is
-        when opcode => "??????";
-    case jump_decision is
-        when opcode => "000001";
-end process;
+        variable opcode : std_logic_vector(5 downto 0) := instruction(31 downto 26);
+	begin
+		case opcode is
+			when "000000" => -- R Format
+				RegDst 			<= '1';
+				ALUSrc 			<= '0';
+				MemToReg 		<= '0';
+				RegWrite 		<= '1';
+				MemRead 			<= '0';
+				MemWrite 		<= '0';
+				ALUOp 			<= "10";
+				jump_decision 	<= '0';
+				beq_control 	<= '0';
+			when "100011" => -- LW
+				RegDst 			<= '0';
+				ALUSrc 			<= '1';
+				MemToReg 		<= '1';
+				RegWrite 		<= '1';
+				MemRead 			<= '1';
+				MemWrite 		<= '0';
+				ALUOp 			<= "00";
+				jump_decision 	<= '0';
+				beq_control 	<= '0';
+			when "101011" => -- SW
+				RegDst 			<= '0';
+				ALUSrc 			<= '1';
+				MemToReg 		<= '0';
+				RegWrite			<= '0';
+				MemRead 			<= '0';
+				MemWrite 		<= '1';
+				ALUOp 			<= "00";
+				jump_decision 	<= '0';
+				beq_control 	<= '0';
+			when "000100" => -- BEQ
+				RegDst 			<= '0';
+				ALUSrc 			<= '0';
+				MemToReg 		<= '0';
+				RegWrite			<= '0';
+				MemRead 			<= '0';
+				MemWrite 		<= '1';
+				ALUOp 			<= "01";
+				jump_decision 	<= '0';
+				beq_control 	<= '1';
+			when "001000" => -- ADDI
+				RegDst 			<= '0';
+				ALUSrc 			<= '1';
+				MemToReg 		<= '0';
+				RegWrite			<= '1';
+				MemRead 			<= '0';
+				MemWrite 		<= '0';
+				ALUOp 			<= "00";
+				jump_decision 	<= '0';
+				beq_control 	<= '0';
+			when "000100" => -- BEQ
+				RegDst 			<= '0';
+				ALUSrc 			<= '0';
+				MemToReg 		<= '0';
+				RegWrite			<= '0';
+				MemRead 			<= '0';
+				MemWrite 		<= '0';
+				ALUOp 			<= "00";
+				jump_decision 	<= '1';
+				beq_control 	<= '0';
+			when others => -- halt and catch fire
+				RegDst 			<= '0';
+				ALUSrc 			<= '0';
+				MemToReg 		<= '0';
+				RegWrite			<= '0';
+				MemRead 			<= '0';
+				MemWrite 		<= '0';
+				ALUOp 			<= "00";
+				jump_decision 	<= '0';
+				beq_control 	<= '0';
+			end case;
+	end process;
 end Behavioral;
 
